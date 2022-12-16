c3eedf876cec25a3d60f139f58704ce0b00bcb5c
Андрей Мринский <Nomarg@yandex.ru>
2022-12-16 16:05:38 +0300

    Добавьте файлы проекта.

---
+++ TAP1/MyForm.cpp
@@ -0,0 +1,11 @@
+
+#include <string>
+#include "MyForm.h"
+#include <Windows.h>
+using namespace TAP1; // Название проекта
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	return 0;
+}
---
+++ TAP1/MyForm.h
@@ -0,0 +1,311 @@
+﻿#pragma once
+#include <string>
+#include <msclr\marshal_cppstd.h>
+#include <iostream>
+
+
+namespace TAP1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label4;
+
+
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(46, 53);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(90, 32);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"F → *";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(46, 85);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(167, 32);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"K→ F1 | K1";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(46, 117);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(208, 32);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"S→ K0 | S0 | S*";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::ActiveBorder;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(52, 199);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(274, 287);
+			this->pictureBox1->TabIndex = 3;
+			this->pictureBox1->TabStop = false;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(499, 74);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(273, 33);
+			this->textBox1->TabIndex = 4;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(499, 127);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(273, 42);
+			this->button1->TabIndex = 5;
+			this->button1->Text = L"Вычислить";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->AcceptsReturn = true;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(499, 199);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(273, 287);
+			this->textBox2->TabIndex = 6;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14, System::Drawing::FontStyle::Bold));
+			this->label4->Location = System::Drawing::Point(522, 39);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(217, 22);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"Введите цепочку языка";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(801, 570);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"Лексический анализ";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	textBox2->Text = L"";
+	int q = 0; //вершины ДКА
+	int j = 0; //итератор символов слова
+	while (j<textBox1->Text->Length)
+	{
+		switch (q)
+		{
+		case 0:
+			if (textBox1->Text[j] == '*')
+			{
+				textBox2->Text += "H==>F \r\n";
+				q++;
+				break;
+			}
+			else
+			{
+				textBox2->Text += "Слово не относится к данной граматике";
+				j = textBox1->Text->Length;
+				break;
+			}
+		case 1:
+			if (textBox1->Text[j] == '1')
+			{
+				textBox2->Text += "F==>K \r\n";
+				q++;
+				break;
+			}
+			else
+			{
+				textBox2->Text += "Слово не относится к данной граматике";
+				j = textBox1->Text->Length;
+				break;
+			}
+
+		case 2:
+			if (textBox1->Text[j] == '0')
+			{
+				textBox2->Text += "K==>S \r\n";
+				q++;
+				break;
+			}
+			else
+			{
+				if (textBox1->Text[j]== '1')
+				{
+					textBox2->Text += "K==>K \r\n";
+					break;
+				}
+				else
+				{
+					textBox2->Text += "Слово не относится к данной граматике";
+					j = textBox1->Text->Length;
+					break;
+				}
+			}
+
+		case 3:
+			if (textBox1->Text[j] == '0')
+			{
+				textBox2->Text += "S==>S \r\n";
+				if (j == (textBox1->Text->Length)-1)
+				{
+					textBox2->Text += "Слово принадлежит данной граматике";
+				}
+				break;
+			}
+			else if (textBox1->Text[j] == '*')
+			{
+				textBox2->Text += "S==>S \r\n";
+				if (j == (textBox1->Text->Length) - 1)
+				{
+					textBox2->Text += "Слово принадлежит данной граматике";
+				}
+				break;
+			}
+			else
+			{
+				textBox2->Text += "Слово не относится к данной граматике";
+				j = textBox1->Text->Length;
+				break;
+				
+			}
+		default:
+			break;
+		}
+	
+		j++;
+	}
+	
+	
+
+	
+
+	
+	
+	
+
+	
+
+	
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
+	
+}
+};
+}
